f7a2e86c1f5e1b12fffe08afcfe84067eff8a7ca
Ayatullah-ahmed <aya.ahmed8760@gmail.com>
2024-10-04 19:34:05 -0700

    Implemented insertionSort function

--- specs/insertion-sort/insertion-sort.test.js
+++ specs/insertion-sort/insertion-sort.test.js
@@ -15,11 +15,25 @@
 
 function insertionSort(nums) {
   // code goes here
+  for (let outerCount = 1; outerCount < nums.length; outerCount++) {
+    const numberToInsert = nums[outerCount]; 
+    let innerCount; 
+    for (innerCount = outerCount - 1; innerCount >= 0; innerCount--) {
+      if (nums[innerCount] > numberToInsert) {
+        nums[innerCount + 1] = nums[innerCount]; 
+      } else {
+        break; 
+      }
+    }
+    nums[innerCount + 1] = numberToInsert;
+  }
+  return nums;
 }
+
 
 // unit tests
 // do not modify the below code
-test.skip("insertion sort", function () {
+test("insertion sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   insertionSort(nums);
   expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
