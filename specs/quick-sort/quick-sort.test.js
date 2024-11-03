62968113fffb9a4dd675bc219072894f6fd93ef6
Ayatullah-ahmed <aya.ahmed8760@gmail.com>
2024-11-03 14:41:22 -0800

    Implemented quickSort function

--- specs/quick-sort/quick-sort.test.js
+++ specs/quick-sort/quick-sort.test.js
@@ -13,12 +13,28 @@
 */
 
 function quickSort(nums) {
-  // code goes here
+  if(nums.length < 2)
+  {
+      return nums;
+  }
+  let pivot = nums[nums.length -1];
+  let rightArr=[];
+  let leftArr=[];
+  for(i =0 ; i <nums.length - 1; i++){
+      if(nums[i] < pivot){
+          leftArr.push(nums[i]);
+      }
+      else{
+          rightArr.push(nums[i]);
+      }
+  }
+  return quickSort(leftArr).concat(pivot).concat(quickSort(rightArr))
+
 }
 
 // unit tests
 // do not modify the below code
-test.skip("quickSort", function () {
+test("quickSort", function () {
   const input = [10, 8, 2, 1, 6, 3, 9, 4, 7, 5];
   const answer = quickSort(input);
 
